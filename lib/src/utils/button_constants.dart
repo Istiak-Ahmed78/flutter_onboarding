3e97c7c2cad3142cc1b933f62e531e5d0a9160ae
Istiak <aistiak2078@gmail.com>
2022-01-04 00:53:11 +0600

    Buttons default values added

---
+++ lib/src/utils/button_constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+///Default Skip Button style
+class DefaultSkipButtonStyle {
+  static const Color defaultButtonColor = Colors.grey;
+  static const Text defaultSkipText = Text('Skip');
+  static const double defaultBorderRaduis = 20.0;
+  static const EdgeInsets defaultPadding = EdgeInsets.all(6);
+}
+
+///Default Procced Button style
+class DeafultPreccedButtonStrle {
+  static const Color defaultButtonColor = Colors.grey;
+  static const Text defaultProccedText = Text('Next');
+  static const double defaultBorderRaduis = 20.0;
+  static const EdgeInsets defaultPadding = EdgeInsets.all(6);
+}
